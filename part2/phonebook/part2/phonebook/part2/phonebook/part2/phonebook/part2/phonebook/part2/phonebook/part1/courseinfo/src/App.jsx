03c0bd52bb9832955f53272b2a9422368e4451b1
kieranlamay <kieran_lamay@brown.edu>
2025-11-12 11:17:16 -0500

    dist w/ url fix

--- part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part1/courseinfo/src/App.jsx
+++ part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part2/phonebook/part1/courseinfo/src/App.jsx
@@ -15,7 +15,10 @@ const Content = (props) => {
     <div>
       <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
       <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part
+        part={props.course.parts[2].name}
+        exercises={props.course.parts[2].exercises}
+      />
     </div>
   );
 };
@@ -24,9 +27,7 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
+      {props.exercises1 + props.exercises2 + props.exercises3}
     </p>
   );
 };
