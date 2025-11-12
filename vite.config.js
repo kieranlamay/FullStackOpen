9333e688422dde5147ab1ffd3f8730a4a369fefb
kieranlamay <kieran_lamay@brown.edu>
2025-11-12 11:15:51 -0500

    baseUrl double slash issue fixed

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://localhost:3001",
-        changeOrigin: true,
+        changeOrigin: true, 
       },
     },
     watch: {
